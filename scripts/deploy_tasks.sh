3902c6d0ac5216332c5a7689c5ddc6bc27f55429
Sergey Spiridonov <jettplayyt@gmail.com>
2025-09-01 07:04:37 +0000

    Split deployment

--- scripts/deploy_tasks.sh
+++ scripts/deploy_tasks.sh
@@ -10,21 +10,3 @@ for file in $CHANGED_FILES; do
 		docker compose -f $directory/docker-compose.yml up --build -d; 
 	fi
 done
-
-CREATED_FILES=$(git diff --name-only --diff-filter=A HEAD~1..HEAD)
-
-for file in $CREATED_FILES; do
-    directory=$( echo "$file" | sed -nE 's/(tasks\/(web|crypto|stego|forensic|osint|joy|reverse|pwn)\/[^\/]+?)\/.+?challenge\.(yaml|yml)/\1/p' )
-    if [ -d "$directory" ]; then
-		ctf challenge install $directory;
-    fi
-done
-
-MODIFIED_FILES=$(git diff --name-only --diff-filter=M HEAD~1..HEAD)
-
-for file in $MODIFIED_FILES; do
-    directory=$( echo "$file" | sed -nE 's/(tasks\/(web|crypto|stego|forensic|osint|joy|reverse|pwn)\/[^\/]+?)\/.+?challenge\.(yaml|yml)/\1/p' )
-    if [ -d "$directory" ]; then
-		ctf challenge sync $directory;
-    fi
-done
