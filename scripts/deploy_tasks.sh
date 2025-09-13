725fb6fb437e6992af07789b3800ad202223e496
Spiridonov Sergey Vladimirovich <sergey_spiridonov@ozon.ru>
2025-09-13 16:29:34 +0300

    Fix deployment script

--- scripts/deploy_tasks.sh
+++ scripts/deploy_tasks.sh
@@ -8,7 +8,7 @@ for file in $CHANGED_FILES; do
 	directory=$(dirname $file)
 	if [[ $directory == */deploy ]]; then
 		echo "Deploying $directory...";
-		docker compose -f $directory/docker-compose.yml up --build -d;
+		docker compose -f $directory/docker-compose.yml down && docker compose -f $directory/docker-compose.yml up --build -d;
 		PORT=$(grep -Eo "[0-9]+:[0-9]+" docker-compose.yml | cut -d: -f1)
 		echo "Deployed $directory on 195.66.114.196:$PORT"
 	fi
